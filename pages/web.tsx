b8071298d16ec392435392a8630394e483f26072
Vũ. Vy Quốc <vyquocvu@users.noreply.github.com>
2023-07-20 17:12:37 +0700

    replace code

---
+++ pages/web.tsx
@@ -0,0 +1,231 @@
+import type { NextPage } from "next";
+
+const Web: NextPage = () => {
+  return (
+    <div className="relative rounded-2xl bg-white-100 w-full h-[1024px] overflow-hidden text-left text-sm text-black-100 font-regular">
+      <div className="absolute h-full top-[0px] bottom-[0px] left-[0px] bg-white-100 box-border w-[282px] border-r-[1px] border-solid border-black-10">
+        <div className="absolute top-[20px] left-[20px] w-[242px] flex flex-col items-start justify-start gap-[4px]">
+          <div className="self-stretch rounded-xl bg-black-100 h-11 flex flex-row py-2 px-4 box-border items-center justify-center gap-[8px] text-center text-lg text-white-100">
+            <img className="relative w-5 h-5" alt="" src="/adds1.svg" />
+            <div className="relative leading-[28px] font-semibold">
+              New chat
+            </div>
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/chatd7.svg" />
+            <div className="relative leading-[20px] z-[1]">
+              Ai Chat Tool Ethics
+            </div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/chatd7.svg" />
+            <div className="relative leading-[20px] z-[1]">
+              Al Chat Tool Impact Writing
+            </div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/chatd7.svg" />
+            <div className="relative leading-[20px] z-[1]">New chat</div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+        </div>
+        <div className="absolute bottom-[20px] left-[20px] w-[242px] flex flex-col items-start justify-start gap-[4px]">
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/trashd2.svg" />
+            <div className="relative leading-[20px] z-[1]">
+              Clear conversations
+            </div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/sund4.svg" />
+            <div className="relative leading-[20px] z-[1]">Light mode</div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img className="relative w-6 h-6 z-[0]" alt="" src="/userd3.svg" />
+            <div className="relative leading-[20px] z-[1]">My account</div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img
+              className="relative w-6 h-6 z-[0]"
+              alt=""
+              src="/arrowsquareoutd2.svg"
+            />
+            <div className="relative leading-[20px] z-[1]">{`Updates & FAQ`}</div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+          <div className="self-stretch rounded-lg flex flex-row p-3 items-center justify-start relative gap-[12px]">
+            <img
+              className="relative w-6 h-6 z-[0]"
+              alt=""
+              src="/signoutd2.svg"
+            />
+            <div className="relative leading-[20px] z-[1]">Log out</div>
+            <img
+              className="absolute my-0 mx-[!important] top-[calc(50%_-_8px)] right-[12px] w-4 h-4 hidden z-[2]"
+              alt=""
+              src="/arrowlinerights8.svg"
+            />
+          </div>
+        </div>
+        <div className="absolute w-[calc(100%_+_1px)] right-[-0.5px] bottom-[295.5px] left-[-0.5px] box-border h-px border-t-[1px] border-solid border-black-10" />
+      </div>
+      <div className="absolute top-[0px] left-[282px] w-[1158px] h-[1024px] overflow-hidden text-center text-29xl">
+        <div className="absolute top-[200px] left-[417px] w-[325px] h-[58px] hidden">
+          <div className="absolute top-[0px] left-[0px] flex flex-row items-center justify-start gap-[16px]">
+            <img
+              className="relative w-12 h-12 overflow-hidden shrink-0"
+              alt=""
+              src="/chatgpt3.svg"
+            />
+            <div className="relative leading-[58px] font-semibold">ChatGPT</div>
+            <div className="rounded bg-secondary-purple-b h-5 flex flex-row py-px px-1 box-border items-center justify-center text-left text-xs">
+              <div className="relative leading-[18px]">Plus</div>
+            </div>
+          </div>
+          <div className="absolute top-[138px] left-[-298px] w-[920px] h-[276px] hidden text-left text-lg">
+            <div className="absolute top-[0px] left-[90px] rounded-lg flex flex-col p-2 items-center justify-start gap-[8px]">
+              <img className="relative w-8 h-8" alt="" src="/chatsr2.svg" />
+              <div className="relative leading-[24px] font-semibold">
+                Examples
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[400px] rounded-lg flex flex-col p-2 items-center justify-start gap-[8px]">
+              <img className="relative w-8 h-8" alt="" src="/stars3.svg" />
+              <div className="relative leading-[24px] font-semibold">
+                Capabilities
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[723px] rounded-lg flex flex-col p-2 items-center justify-start gap-[8px]">
+              <img
+                className="relative w-8 h-8"
+                alt=""
+                src="/shieldwarningr2.svg"
+              />
+              <div className="relative leading-[24px] font-semibold">
+                Limitations
+              </div>
+            </div>
+            <div className="absolute top-[100px] left-[0px] w-[280px] flex flex-col items-start justify-start gap-[16px] text-sm">
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  "Explain quantum computing insimple terms"
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  "Got any creative ideas for a 10year old's birthday?"
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  "How do I make an HTTP requestin Javascript?"
+                </div>
+              </div>
+            </div>
+            <div className="absolute top-[100px] left-[320px] w-[280px] flex flex-col items-start justify-start gap-[16px] text-sm">
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  Remembers what user saidearlier in the conversation.
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  Allows user to provide follow-up corrections.
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  Trained to decline inappropriate requests.
+                </div>
+              </div>
+            </div>
+            <div className="absolute top-[100px] left-[640px] w-[280px] flex flex-col items-start justify-start gap-[16px] text-sm">
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  May occasionally generate incorrect information.
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  May occasionally produce harmful instructions or biased
+                  content.
+                </div>
+              </div>
+              <div className="self-stretch rounded-lg bg-primary-light flex flex-row py-1 px-2 items-center justify-start">
+                <div className="flex-1 relative leading-[20px]">
+                  Limited knowledge of world andevents after 2021.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute bottom-[0px] left-[calc(50%_-_579px)] bg-white-80 [backdrop-filter:blur(20px)] w-[1158px] h-28 overflow-hidden flex flex-col pt-5 px-0 pb-10 box-border items-center justify-start text-left text-sm text-black-20">
+          <div className="relative rounded-2xl bg-primary-light w-[758px] h-[52px]">
+            <div className="absolute top-[16px] left-[92px] leading-[20px]">
+              Type message
+            </div>
+            <img
+              className="absolute top-[calc(50%_-_8px)] right-[18px] w-4 h-4 hidden"
+              alt=""
+              src="/warningr3.svg"
+            />
+            <div className="absolute top-[12px] right-[12px] rounded-lg w-7 h-7 flex flex-row p-1 box-border items-center justify-center">
+              <img
+                className="relative w-5 h-5"
+                alt=""
+                src="/paperplanerightr2.svg"
+              />
+            </div>
+            <div className="absolute top-[12px] left-[12px] flex flex-row items-center justify-start gap-[8px]">
+              <div className="rounded-lg w-7 h-7 flex flex-row p-1 box-border items-center justify-center">
+                <img
+                  className="relative w-5 h-5"
+                  alt=""
+                  src="/microphoner3.svg"
+                />
+              </div>
+              <div className="rounded-lg w-7 h-7 flex flex-row p-1 box-border items-center justify-center">
+                <img className="relative w-5 h-5" alt="" src="/imager2.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Web;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        gray: {
-          "100": "#797979",
-          "200": "#212121",
-          "300": "#121212",
-          "400": "rgba(255, 255, 255, 0.5)",
-          "500": "rgba(18, 18, 18, 0.64)",
-          "600": "rgba(255, 255, 255, 0.72)",
-        },
-        white: "#fff",
-        dimgray: "#4d4d4d",
-        forestgreen: "#1db954",
+        "white-10": "rgba(255, 255, 255, 0.1)",
+        "white-20": "rgba(255, 255, 255, 0.2)",
+        "white-5": "rgba(255, 255, 255, 0.05)",
+        "black-5": "rgba(0, 0, 0, 0.05)",
+        "black-100": "#1c1c1c",
+        "white-80": "rgba(255, 255, 255, 0.8)",
+        "primary-light": "#f7f9fb",
+        "black-20": "rgba(0, 0, 0, 0.2)",
+        "secondary-purple-b": "#c6c7f8",
+        "white-100": "#fff",
+        "black-10": "rgba(0, 0, 0, 0.1)",
       },
       fontFamily: {
-        manrope: "Manrope",
+        regular: "Inter",
       },
       borderRadius: {
-        "21xl": "40px",
-        "3xs": "10px",
-        "10xs": "3px",
+        "xs-1": "11.1px",
+        "7xs-6": "5.6px",
+        "10xs-8": "2.8px",
+        "4xs-4": "8.4px",
       },
     },
     fontSize: {
+      "3xs-7": "9.7px",
+      "smi-5": "12.5px",
+      "4xs-4": "8.4px",
+      "14xl-4": "33.4px",
       sm: "14px",
-      lg: "18px",
-      base: "16px",
       xs: "12px",
-      xl: "20px",
+      lg: "18px",
+      "29xl": "48px",
     },
   },
   corePlugins: {
